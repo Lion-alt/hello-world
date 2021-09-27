f724751e57912c4db354c1079d29e8cac375a09a
Lion-alt <lionbee@hispeed.ch>
2021-09-27 16:28:34 +0200

    feature A - new attribute

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,9 @@
 public class HelloWorld {
-    
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
